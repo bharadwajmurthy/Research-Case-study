0bbb044a937f23beadbf53ff7d68595cc828269f
Duckeater1 <longweiyu@outlook.com>
2023-11-27 13:10:57 +0100

    Update Quasi Likelihood estimator vs Pseudo likelihood estimator.py

--- Quasi Likelihood estimator vs Pseudo likelihood estimator.py
+++ Quasi Likelihood estimator vs Pseudo likelihood estimator.py
@@ -1,32 +1,63 @@
-```Python
-import matplotlib.pyplot as plt
 
-# Plot the histogram of the lognormal data
+```python
+from sklearn.mixture import GaussianMixture
+
+# Parameters for the mixture model
+mu1, sigma1 = -2, 0.5  # Parameters for the first normal distribution
+mu2, sigma2 = 2, 1.5   # Parameters for the second normal distribution
+mixture_proportions = [0.3, 0.7]  # Proportions of each mixture component
+
+# Generate synthetic data from a mixture of two normal distributions
+np.random.seed(0)
+data_mixture = np.concatenate([
+    np.random.normal(mu1, sigma1, int(10000 * mixture_proportions[0])),
+    np.random.normal(mu2, sigma2, int(10000 * mixture_proportions[1]))
+])
+
+# PLE: Fit a Gaussian mixture model (we use this as PLE here for simplicity)
+# This is actually a full likelihood approach for mixture models, 
+# but we can consider it a PLE since we maximize the likelihood for each component separately
+gmm = GaussianMixture(n_components=2, covariance_type='full')
+gmm.fit(data_mixture.reshape(-1, 1))
+
+# QLE: Calculate the sample mean and variance (not using a specialized QLE method here, for simplicity)
+qle_mean = np.mean(data_mixture)
+qle_variance = np.var(data_mixture)
+
+# Sort the mixture components by mean for easier comparison
+sorted_indices = np.argsort(gmm.means_.ravel())
+ple_means = gmm.means_.ravel()[sorted_indices]
+ple_variances = gmm.covariances_.ravel()[sorted_indices]
+
+# Plot the histogram of the mixture data
 plt.figure(figsize=(12, 6))
-plt.hist(lognormal_data, bins=30, alpha=0.6, color='g', label='Log-normal Data')
-plt.axvline(x=qle_median, color='b', linestyle='--', label='QLE Median (Approx. Mu)')
-plt.axvline(x=np.exp(true_lognormal_mu), color='r', linestyle='-', label='True Mu')
-plt.title('Histogram of Log-normal Data and Estimates')
+plt.hist(data_mixture, bins=50, alpha=0.6, color='g', label='Mixture Data')
+plt.axvline(x=qle_mean, color='b', linestyle='--', label='QLE Mean')
+plt.title('Histogram of Mixture Data and QLE Estimate')
 plt.xlabel('Value')
 plt.ylabel('Frequency')
 plt.legend()
 plt.show()
 
-# Plot for comparison of MLE vs QLE
+# Plot for comparison of PLE vs QLE
 plt.figure(figsize=(12, 6))
 
 # True parameters
-plt.scatter(true_lognormal_mu, true_lognormal_sigma, color='r', zorder=5, label='True Parameters')
+plt.scatter([mu1, mu2], [sigma1, sigma2], color='r', zorder=5, label='True Parameters')
 
-# MLE Estimates
-plt.scatter(mle_normal_mu, mle_normal_sigma, color='g', label='MLE Estimates (Assuming Normality)')
+# PLE Estimates
+plt.scatter(ple_means, np.sqrt(ple_variances), color='g', label='PLE Estimates')
 
-# QLE Estimates
-plt.scatter(qle_approx_mu, qle_approx_sigma, color='b', label='QLE Estimates')
+# QLE Estimate (only one mean and variance since QLE does not consider the mixture)
+plt.scatter(qle_mean, np.sqrt(qle_variance), color='b', label='QLE Estimate')
 
-plt.title('Comparison of Parameter Estimates')
-plt.xlabel('Mu Estimate')
-plt.ylabel('Sigma Estimate')
+plt.title('Comparison of Parameter Estimates for Mixture Data')
+plt.xlabel('Mean Estimate')
+plt.ylabel('Standard Deviation Estimate')
 plt.legend()
 plt.grid(True)
 plt.show()
+
+# Return the estimates for comparison
+(ple_means, np.sqrt(ple_variances)), (qle_mean, np.sqrt(qle_variance))
+```
